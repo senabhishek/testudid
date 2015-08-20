8fd1393f4183da7e05079d9767d19336d8ae7be6
senabhishek <sen.abhishek@gmail.com>
2015-08-20 02:24:38 +0200

    Initial Commit

---
+++ TestUDID/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestUDID
+//
+//  Created by Abhishek Sen on 8/20/15.
+//  Copyright (c) 2015 Abhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
